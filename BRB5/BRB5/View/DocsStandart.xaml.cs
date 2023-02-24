10ae9d228f68e01676e2c68b389c4758ba9ed3ce
Alina Rutkovska <anilarut@gmail.com>
2023-02-24 15:05:18 +0200

    прихід фільтр за окпо і штрихкодом

--- BRB5/BRB5/View/DocsStandart.xaml.cs
+++ BRB5/BRB5/View/DocsStandart.xaml.cs
@@ -17,17 +17,21 @@ namespace BRB5.View
         private Connector.Connector c=Connector.Connector.GetInstance();
         private TypeDoc TypeDoc;
         DB db = DB.GetDB();
-        public ObservableCollection<Doc> MyDocsR { get; set; }
+        private ObservableCollection<Doc> _MyDocsR;
+        public ObservableCollection<Doc> MyDocsR { get { return _MyDocsR; } set { _MyDocsR = value; OnPropertyChanged("MyDocsR"); } }
         bool _IsVisOPKO = false;
         public bool IsVisOPKO { get { return _IsVisOPKO; } set { _IsVisOPKO = value; OnPropertyChanged("IsVisOPKO"); } }
         string _OPKOstr = "";
         public string OPKOstr { get { return _OPKOstr; } set { _OPKOstr = value; OnPropertyChanged("OPKOstr"); } }
+
+        bool _IsVisBarCode = false;
+        public bool IsVisBarCode { get { return _IsVisBarCode; } set { _IsVisBarCode = value; OnPropertyChanged("IsVisBarCode"); } }
         public bool IsViewOut { get { return TypeDoc.IsViewOut; } }
 
 
         public DocsStandart(TypeDoc pTypeDoc )
         {
-            TypeDoc = pTypeDoc;            
+            TypeDoc = pTypeDoc;
             BindingContext = this;
             InitializeComponent();
         }
@@ -48,9 +52,28 @@ namespace BRB5.View
             await Navigation.PushAsync(new DocStandart(vDoc,TypeDoc));
         }
 
-        private void F1(object sender, EventArgs e)
+        private void OKPO(object sender, EventArgs e)
         {
             IsVisOPKO = !IsVisOPKO;
+        }
+
+        private void FilterDocs(object sender, EventArgs e)
+        {
+            if (OPKOstr.Length > 2)
+                MyDocsR = new ObservableCollection<Doc>(db.GetDoc(TypeDoc, null, OPKOstr));
+        }
+
+        private void BarCode(object sender, EventArgs e)
+        {
+            IsVisBarCode= !IsVisBarCode;
+            zxing.IsScanning = IsVisBarCode;
+        }
+
+        private void FilterBarCode(ZXing.Result result)
+        {
+            zxing.IsAnalyzing = false;
+            MyDocsR = new ObservableCollection<Doc>(db.GetDoc(TypeDoc, result.Text, null));
+            zxing.IsAnalyzing = true;
         }
     }
 }
