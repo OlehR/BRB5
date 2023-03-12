c4dfbaba35d114f1a2bc65928cacd369c16c0ea2
Oleg Rutkovskyi <OlehRut@gmail.com>
2023-03-12 09:01:40 +0200

    5.0.0.26 GPS

--- BRB5/BRB5.Android/Properties/AssemblyInfo.cs
+++ BRB5/BRB5.Android/Properties/AssemblyInfo.cs
@@ -24,8 +24,8 @@ using Android.App;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("5.0.0.25")]
-[assembly: AssemblyFileVersion("5.0.0.25")]
+[assembly: AssemblyVersion("5.0.0.26")]
+[assembly: AssemblyFileVersion("5.0.0.26")]
 
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
