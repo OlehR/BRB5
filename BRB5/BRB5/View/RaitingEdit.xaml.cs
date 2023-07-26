5be4c3277f8a68d38c1d0e3435301044978f35f3
Alina Rutkovska <anilarut@gmail.com>
2023-07-26 13:50:21 +0300

    +склад при формуванні опитування

--- BRB5/BRB5/View/RaitingEdit.xaml.cs
+++ BRB5/BRB5/View/RaitingEdit.xaml.cs
@@ -22,6 +22,28 @@ namespace BRB5.View
         public RaitingTemplate SelectedTemplate { get { return _SelectedTemplate; } set { _SelectedTemplate = value; OnPropertyChanged(nameof(SelectedTemplate)); } }
         public List<RaitingTemplate> RT { get { return db.GetRaitingTemplate().ToList(); } }
         public Doc RD { get { return _RD; } set { _RD = value; OnPropertyChanged(nameof(RD)); } }
+        public List<Warehouse> ListWarehouse
+        {
+            get
+            {
+                List<Warehouse> wh = null;
+                try
+                {
+                    wh = db.GetWarehouse()?.ToList();
+
+                }
+                catch (Exception ex)
+                {
+                    string msg = ex.Message;
+                }
+                if (wh == null || !wh.Any())
+                    wh = new List<Warehouse>() { new Warehouse() { Code = 0, Name = "ddd" } };
+                return wh;
+
+            }
+        }
+        private Warehouse _SelectedWarehouse;
+        public Warehouse SelectedWarehouse { get { return _SelectedWarehouse; } set { _SelectedWarehouse = value; OnPropertyChanged(nameof(SelectedWarehouse)); } }
         public RaitingEdit (Doc doc, TypeDoc vTypeDoc)
 		{
 			InitializeComponent ();
@@ -34,6 +56,9 @@ namespace BRB5.View
         private async void Save(object sender, EventArgs e)
         {
             RD.IdTempate = SelectedTemplate.Id;
+            RD.CodeWarehouse = SelectedWarehouse.CodeWarehouse;
+
+
             await Navigation.PushAsync(new RaitingDocs(TypeDoc));
 
         }
