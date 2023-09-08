3dab9c257b5fdbaef01dc9b66865e7628b38489e
Alina Rutkovska <anilarut@gmail.com>
2023-09-08 12:39:22 +0300

    WareInfo

---
+++ BRB5/BRB5/View/WareInfo.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BRB5.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WareInfo : ContentPage
+    {
+        DB db = DB.GetDB();
+        public WareInfo()
+        {
+            InitializeComponent();
+            this.BindingContext = this;
+        }
+    }
+}
