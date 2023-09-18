a7d884b6e7c3a90dc7e2becc4523bbcfd174400e
Oleg Rutkovskyi <OlehRut@gmail.com>
2023-09-18 16:14:27 +0300

    move api

---
+++ Model/PSU.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BRB5.Model
+{
+    public class Api
+    {
+        public int CodeData { get; set; }
+        public string Login { get; set; }
+        public string PassWord { get; set; }
+        public string SerialNumber { get; set; }
+        public string NameDCT { get; set; }
+        public int Ver { get; set; }
+        public int CodeWarehouse { get; set; }
+        public Api(int pCodeData) : this()
+        {
+            CodeData = pCodeData;
+        }
+        public Api()
+        {
+            CodeWarehouse = Config.CodeWarehouse;
+            Login = Config.Login;
+            PassWord = Config.Password;
+            Ver = Config.Ver;
+            SerialNumber = Config.SN;
+        }
+    }
+
+    public class ApiPrice : Api
+    {
+        public ApiPrice() : base() { }
+        public ApiPrice(int pCodeData, ParseBarCode pPBC) : base(pCodeData)
+        {
+            BarCode = pPBC.BarCode;
+            CodeWares = pPBC.CodeWares;
+            Article = pPBC.Article;
+        }
+        public string BarCode { get; set; }
+        public int CodeWares { get; set; }
+        public int Article { get; set; }
+    }
+
+    public class ApiDoc : Api
+    {
+        public ApiDoc() : base() { }
+        public ApiDoc(int pCodeData, int pTypeDoc, string pNumberDoc = null) : base(pCodeData)
+        {
+            TypeDoc = pTypeDoc;
+            NumberDoc = pNumberDoc;
+        }
+        public int TypeDoc { get; set; }
+        public string NumberDoc { get; set; }
+    }
+
+    public class ApiSaveDoc : ApiDoc
+    {
+        public ApiSaveDoc() : base() { }
+        public ApiSaveDoc(int pCodeData, int pTypeDoc, string pNumberDoc = null, IEnumerable<object> pWares = null) : base(pCodeData, pTypeDoc, pNumberDoc)
+        {
+            Wares = pWares;
+        }
+        public IEnumerable<object> Wares { get; set; }
+
+    }
+    public class ApiLogPrice : Api
+    {
+        public ApiLogPrice() : base() { }
+        public ApiLogPrice(IEnumerable<object[]> pLogPrice) : base(141)
+        {
+            LogPrice = pLogPrice;
+        }
+        public IEnumerable<object[]> LogPrice { get; set; }
+    }
+
+    public class ApiPrintHTTP : Api
+    {
+        public ApiPrintHTTP() : base() { }
+        public ApiPrintHTTP(string pWares = null) : base(999)
+        {
+            CodeWares = pWares;
+        }
+        public string CodeWares { get; set; }
+
+    }
+}
