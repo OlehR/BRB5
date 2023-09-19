d5356f8750baf92f3a265a3b0434eadfe09b827f
Oleg Rutkovskyi <OlehRut@gmail.com>
2023-09-19 18:12:17 +0300

    xxx

--- BRB5/BRB5.Android/MainActivity.cs
+++ BRB5/BRB5.Android/MainActivity.cs
@@ -54,12 +54,13 @@ namespace BRB5.Droid
             ///!!!!=TMP копіювання бази
             try
             {
-                string CopyTo = Path.Combine(Config.PathDownloads, "brb5.db");
-                if (File.Exists(CopyTo))
-                    File.Delete(CopyTo);
-                File.Copy(db.PathNameDB, CopyTo, true);
-                //File.Create(CopyTo);
-                //File.WriteAllBytes(CopyTo, b);
+                //string CopyTo = Path.Combine(Config.PathDownloads, "brb5.db");
+                //if (File.Exists(CopyTo)) File.Delete(CopyTo);
+               // File.Copy(db.PathNameDB, CopyTo, true);
+
+                  string path1 = Path.Combine(Android.OS.Environment.ExternalStorageDirectory.AbsolutePath, Android.OS.Environment.DirectoryDownloads);
+                File.Copy(db.PathNameDB, Path.Combine(path1, "brb5.db") , true);
+                
             }
             catch (Exception e)
             {
