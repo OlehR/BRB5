414904553cd9257cec9f619bce3043813311dd85
Oleg Rutkovskyi <OlehRut@gmail.com>
2023-10-10 16:28:20 +0300

    Чергова збірка

--- BRB5/BRB5.Android/Properties/AssemblyInfo.cs
+++ BRB5/BRB5.Android/Properties/AssemblyInfo.cs
@@ -24,8 +24,8 @@ using Android.App;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("5.0.0.36")]
-[assembly: AssemblyFileVersion("5.0.0.36")]
+[assembly: AssemblyVersion("5.0.0.38")]
+[assembly: AssemblyFileVersion("5.0.0.38")]
 
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
