d0b9e1d7b5ff1f35d93ae81c109b5e2ec20ad314
Alina Rutkovska <anilarut@gmail.com>
2023-10-18 16:38:29 +0200

    x

--- BRB5/BRB5/View/WareInfo.xaml.cs
+++ BRB5/BRB5/View/WareInfo.xaml.cs
@@ -16,8 +16,6 @@ namespace BRB5.View
         DB db = DB.GetDB(); 
         Connector.Connector c;
         public WaresPrice WP { get; set; }
-        private string _Promotion;
-        public string Promotion { get { return _Promotion; } set { _Promotion = value; OnPropertyChanged(nameof(Promotion)); } }
         public bool IsVisPromotion {  get; set; }  = false;
         public string ImageUri { get; set; } = "Photo.png"; 
         public bool IsEnabledPrint { get { return Config.TypeUsePrinter != eTypeUsePrinter.NotDefined; } }
@@ -29,12 +27,8 @@ namespace BRB5.View
             
             WP = c.GetPrice(parseBarCode, eTypePriceInfo.Full);
            
-            if (WP.ActionType >0) 
-            {
-                Promotion = $"Акція діє: з {WP.PromotionBegin:dd.MM}  по {WP.PromotionEnd:dd.MM}";
-                IsVisPromotion = true;
-            }
-
+            if (WP.ActionType > 0)  IsVisPromotion = true;
+            
             ImageUri = Config.ApiUrl1+$"Wares/{WP.CodeWares:D9}.png";
             WareImage.Source = new UriImageSource
             {
