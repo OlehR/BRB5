e63c747831510b43577695380fad170150f818ed
Alina Rutkovska <anilarut@gmail.com>
2024-02-02 17:00:20 +0200

    x

---
+++ BRB5/BRB5/ViewModel/DocWaresEx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace BRB5.ViewModel
+{
+    public static class DocWaresEx
+    {    
+       static public Keyboard Keyboard(this BRB5.Model.DocWaresEx DWE) { 
+            return DWE.CodeUnit == 7 ? Xamarin.Forms.Keyboard.Telephone : Xamarin.Forms.Keyboard.Numeric;  }
+    }
+    
+}
