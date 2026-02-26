3a7801177e7bc027ddaee97295e4e26a98edbc00
Олег Рутковський <OlehR@outlook.com>
2026-02-26 16:35:34 +0200

    6.1.36

--- BL/DB.cs
+++ BL/DB.cs
@@ -1048,9 +1048,9 @@ and bc.BarCode=?
         public IEnumerable<DocExpiration> GetDocExpiration()
         {
             string sql = @"select gw.CodeGroup as NumberDoc,gw.NameGroup as Description,count(*) as Count,sum(d.CountInput) as CountInput from
-(select DES.DocId,DES.CodeWares, case when de.DocId is  null then 0 else 1 end as CountInput
+(select DES.DocId,DES.CodeWares, case when de.DocId is  null and  des.QuantityInput=0 then 0 else 1 end as CountInput
  from DocWaresExpirationSample DES 
-   left join DocWaresExpiration DE on DES.CodeWares=DE.CodeWares and DE.DocId=DES.DocId and DATE(DE.DateDoc) = DATE('now')                                                               
+   left join DocWaresExpiration DE on DES.CodeWares=DE.CodeWares and DE.DocId=DES.DocId and DATE(DE.DateDoc) = DATE('now')                                              
                         union all 
         select  DE.DocId,DE.CodeWares,1 as nn
                                 from DocWaresExpiration DE
@@ -1131,7 +1131,7 @@ order by gw.NameGroup";
             string sql = @"select DES.OrderDoc, DES.NumberDoc,DES.DocId, w.CodeWares,w.NameWares as NameWares, au.Coefficient as Coefficient,w.CodeUnit as CodeUnit, ud.ABRUNIT as NameUnit,
                             ( select group_concat(bc.BarCode,',') from BarCode bc where bc.CodeWares=w.CodeWares ) as BARCODE  ,w.CodeUnit as BaseCodeUnit,
                             des.Quantity,des.Expiration,des.ExpirationDate,coalesce( des.DaysLeft, w.DaysLeft) as DaysLeft
-,DE.ExpirationDateInput, coalesce(DE.QuantityInput,case when DES.QuantityInput>0 then DES.QuantityInput else null end) as QuantityInput
+,coalesce(DE.ExpirationDateInput,des.ExpirationDate) as ExpirationDateInput, coalesce(DE.QuantityInput,case when DES.QuantityInput>0 then DES.QuantityInput else null end) as QuantityInput
                                 from WARES w 
                                 join ADDITIONUNIT au on w.CODEWARES=au.CODEWARES and au.CODEUNIT=w.CODEUNIT 
                                 join UNITDIMENSION ud on w.CODEUNIT=ud.CODEUNIT 
