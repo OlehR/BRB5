59d34d51589e21fa2ce4a71d36845f6a756f7542
Олег Рутковський <OlehR@outlook.com>
2026-03-30 13:25:08 +0300

    перехід MVVM початок

---
+++ BRB5.1/View/PriceCheckSim23.xaml.cs
@@ -0,0 +1,155 @@
+﻿using BRB5.Model;
+using BRB6.View;
+using BL;
+using BRB5;
+using BarcodeScanning;
+using CommunityToolkit.Maui.Core.Platform;
+using UtilNetwork;
+using BRB6.ViewModel;
+
+
+
+
+#if ANDROID
+using Android.Views;
+#endif
+
+//using BRB5.Connector;
+namespace BRB6
+{
+    public partial class PriceCheckSim23:ForMVVM
+    {
+        PriceCheckVM VM;
+        CameraView BarcodeScaner;
+        double _PB = 0;
+        public double PB { get { return _PB; } set { _PB = value; OnPropertyChanged(nameof(PB)); } }
+
+        public PriceCheckSim23(TypeDoc pTypeDoc)
+        {
+            InitializeComponent();
+            NokeyBoard();
+            NavigationPage.SetHasNavigationBar(this, DeviceInfo.Platform == DevicePlatform.iOS);
+            VM = new PriceCheckVM(pTypeDoc,this);
+            this.BindingContext = VM;
+     
+        this.Unloaded += UnloadedEvent;
+        }
+        void UnloadedEvent(object sender, EventArgs e)
+        {
+            VM.UnloadedEvent();
+        }
+
+        void Progress(double pProgress) => MainThread.BeginInvokeOnMainThread(() => PB = pProgress);
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (!Config.IsSoftKeyboard)
+            {
+#if ANDROID
+                MainActivity.Key += OnPageKeyDown;
+#endif
+            }
+            if (Config.IsVisScan)
+            {
+                BarcodeScaner = Helper.GetCameraView(true);
+
+                BarcodeScaner.OnDetectionFinished += CameraView_OnDetectionFinished;
+
+                GridZxing.Children.Add(BarcodeScaner);
+
+            }
+            if (!Config.IsVisScan)
+                BarCodeInput.Focus();
+            //if (IsVisDoubleScan && WP!=null) WP.StateDoubleScan = eCheckWareScaned.Nothing;
+            Config.OnProgress += Progress;
+        }
+        private void CameraView_OnDetectionFinished(object sender, BarcodeScanning.OnDetectionFinishedEventArg e)
+        {
+            if (e.BarcodeResults.Length > 0)
+            {
+                BarcodeScaner.PauseScanning = true;
+                VM.FoundWares(e.BarcodeResults[0].DisplayValue);
+                Task.Run(async () =>
+                {
+                    await Task.Delay(1000);
+                    BarcodeScaner.PauseScanning = false;
+                });
+            }
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            if (Config.IsVisScan) BarcodeScaner.CameraEnabled = false;
+
+            if (!Config.IsSoftKeyboard)
+            {
+#if ANDROID
+                MainActivity.Key -= OnPageKeyDown;
+#endif
+            }
+            VM.bl.SendLogPrice();
+            Config.OnProgress -= Progress;
+        }
+
+        private async void OnClickWareInfo(object sender, EventArgs e)
+        {
+            if (VM.WP != null)
+            {
+                if (Config.IsVisScan) BarcodeScaner.PauseScanning = true;
+                await Navigation.PushAsync(new WareInfo(VM.WP.ParseBarCode));
+            }
+        }
+
+        public void Focused(string pName)
+        {
+            if (pName == "BarCodeInput")
+                BarCodeFocused(null, null);
+            else
+                NumberOfReplenishment.Focus();
+        }
+
+        public void DisplayAlert(string title, string message, string cancel)
+        {
+            Dispatcher.Dispatch(() => DisplayAlert(title, message, cancel));
+        }
+
+        private void BarCodeFocused(object sender, FocusEventArgs e)
+        {
+            Dispatcher.Dispatch(() =>
+            {
+                BarCodeInput.CursorPosition = 0;
+                BarCodeInput.SelectionLength = BarCodeInput.Text == null ? 0 : BarCodeInput.Text.Length;
+                if (!BarCodeInput.IsFocused || Config.IsVisScan)
+                    BarCodeInput.Focus();
+            });
+        }
+#if ANDROID
+        public void OnPageKeyDown(Keycode keyCode, KeyEvent e)
+        {
+         /*  switch (keyCode)
+           {
+            case Keycode.F1:
+            OnClickPrintBlock(null, EventArgs.Empty);
+               return;
+            case Keycode.F2:
+            OnF2(null, EventArgs.Empty);
+               return;
+            case Keycode.F4:
+            OnF4(null, EventArgs.Empty);
+               return;
+            case Keycode.F5:
+            OnF5(null, EventArgs.Empty);
+               return;
+            case Keycode.F6:
+            OnClickPrintOne(null, EventArgs.Empty);
+               return;
+
+            default:
+               return;
+           }*/
+         }
+#endif
+    }
+}
---
+++ BRB5.1/ViewModel/PriceCheckVM.cs
@@ -0,0 +1,281 @@
+﻿using BarcodeScanning;
+using BL;
+using BRB5;
+using BRB5.Model;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UtilNetwork;
+
+namespace BRB6.ViewModel
+{
+    interface ForMVVM {
+        void Focused(string pName);
+        void DisplayAlert(string title, string message, string cancel);
+    }
+    internal class PriceCheckVM: ObservableObject, IDisposable
+    {
+        ForMVVM ForMVVM;
+        DB db = DB.GetDB();
+        public BL.BL bl = BL.BL.GetBL();
+
+        public List<PrintBlockItems> ListPrintBlockItems { get { return db.GetPrintBlockItemsCount().ToList(); } }
+
+        public int SelectedPrintBlockItems { get { return ListPrintBlockItems.Count > 0 ? ListPrintBlockItems.Last().PackageNumber : -1; } }
+        public bool IsVisPriceNormal { get { return WP != null && (WP.PriceOld != WP.PriceNormal) && WP.PriceNormal != 0; } }
+        public bool IsVisPriceOpt { get { return WP != null && (WP.PriceOpt != 0 || WP.PriceOptOld != 0); } }
+        public bool IsVisPriceOptQ { get { return WP != null && WP.QuantityOpt != 0; } }
+
+        public bool IsVisF4 { get { return Config.LocalCompany == eCompany.Sim23; } }
+        public string F4Text { get { return IsOnline ? "OnLine" : "OffLine"; } }
+        private bool _IsOnline = true;
+        public bool IsOnline { get { return _IsOnline; } set { _IsOnline = value; OnPropertyChanged(nameof(F4Text)); } }
+
+        bool _IsVisRepl = false;
+        public bool IsVisRepl { get { return _IsVisRepl; } set { _IsVisRepl = value; OnPropertyChanged(nameof(IsVisRepl)); } }
+        public bool IsSoftKeyboard { get { return Config.IsSoftKeyboard; } }
+        
+
+        WaresPrice _WP;
+        public WaresPrice WP
+        {
+            get { return _WP; }
+            set
+            {
+                _WP = value; OnPropertyChanged(nameof(WP)); OnPropertyChanged(nameof(TextColorPrice));
+                OnPropertyChanged("IsVisPriceOpt"); OnPropertyChanged(nameof(IsVisPriceNormal)); OnPropertyChanged(nameof(TextColorHttp));
+                OnPropertyChanged("ColorPrintColorType"); OnPropertyChanged(nameof(IsVisPriceOptQ));
+            }
+        }
+        //ZXingScannerView zxing;
+        //ZXingDefaultOverlay overlay;
+
+        int _PrintType = 0;//Колір чека 0-звичайний 1-жовтий, -1 не розділяти.        
+        public int PrintType { get { return _PrintType; } set { _PrintType = value; OnPropertyChanged(nameof(PrintType)); OnPropertyChanged(nameof(ColorPrintColorType)); } }
+        public bool IsEnabledPrint { get { return Config.TypeUsePrinter != eTypeUsePrinter.NotDefined; } }
+        /// <summary>
+        /// Номер сканування цінників за день !!!TMP Треба зберігати в базі.
+        /// </summary>
+        int LineNumber = 0;
+        public int AllScan { get; set; } = 0;
+        public int BadScan { get; set; } = 0;
+        /// <summary>
+        /// Номер пакета цінників за день !!!TMP Треба зберігати в базі.
+        /// </summary>
+        int _PackageNumber = 1;
+        public int PackageNumber { get { return _PackageNumber; } set { _PackageNumber = value; OnPropertyChanged(nameof(PackageNumber)); OnPropertyChanged(nameof(ListPrintBlockItems)); OnPropertyChanged(nameof(SelectedPrintBlockItems)); } }
+
+
+        //public int ColorPrintColorType() { return Color.parseColor(HttpState != eStateHTTP.HTTP_OK ? "#ffb3b3" : (PrintType == 0 ? "#ffffff" : "#3fffff00")); }
+
+        public string ColorPrintColorType { get { return WP == null ? "#ffffff" : WP.MinQuantity == 0 ? "#ffd8d8" : WP.ActionType > 0 ? "#F0DC82" : "#ffffff"; } }
+        public string TextColorPrice { get { return (WP != null && WP.Price != 0 && WP.Price == WP.PriceOld && WP.PriceOpt == WP.PriceOptOld) ? "#009800" : "#ff5c5c"; } set { OnPropertyChanged(nameof(TextColorPrice)); } }
+        public string TextColorHttp { get { return (bl.LastResult != null && bl.LastResult.StateHTTP == eStateHTTP.HTTP_OK) ? "#009800" : "#ff5c5c"; } }
+
+        public bool _IsMultyLabel = false;
+        public bool IsMultyLabel { get { return _IsMultyLabel; } set { _IsMultyLabel = value; OnPropertyChanged(nameof(IsMultyLabel)); OnPropertyChanged(nameof(F5Text)); } }
+        public string F5Text { get { return IsMultyLabel ? "Дубл." : "Унік."; } }
+        public bool IsVisScan { get { return Config.IsVisScan; } }
+        /// <summary>
+        /// 0 - нічого , 1 - сканований цінник, 2 - сканований товар, 3 - штрихкод товату не підходить, 4 - цінник не підходить, 5 - успішно
+        /// </summary>
+        private eCheckWareScaned _IsWareScaned = eCheckWareScaned.Nothing;
+        public eCheckWareScaned IsWareScaned { get { return _IsWareScaned; } set { _IsWareScaned = value; OnPropertyChanged(nameof(ColorDoubleScan)); OnPropertyChanged(nameof(IsWareScaned)); /*OnPropertyChanged(nameof(ButtonDoubleScan));*/ OnPropertyChanged(nameof(MessageDoubleScan)); } }
+        public bool IsVisDoubleScan { get; set; }
+        public bool IsVisBarcode { get { return !IsVisDoubleScan; } }
+        // private string _MessageDoubleScan;
+        public string MessageDoubleScan { get { return EnumMethods.GetDescription(WP?.StateDoubleScan ?? eCheckWareScaned.Success); } } //set {  OnPropertyChanged(nameof(MessageDoubleScan)); } }
+        //public string ButtonDoubleScan { get { return IsWareScaned == eCheckWareScaned.Nothing || IsWareScaned == eCheckWareScaned.Success ? "" :  IsWareScaned == eCheckWareScaned.WareScaned || IsWareScaned == eCheckWareScaned.PriceTagNotFit ? "Відсутній ціник" : "Відсутній товар"; } }
+        public string ColorDoubleScan
+        {
+            get
+            {
+                return IsWareScaned == eCheckWareScaned.Success ? "#C5FFC4" : IsWareScaned == eCheckWareScaned.Bad || IsWareScaned == eCheckWareScaned.BadPrice ? "#FFC4C4" :
+                                                     IsWareScaned == eCheckWareScaned.PriceTagScaned || IsWareScaned == eCheckWareScaned.WareScaned ? "#FEFFC4" : "#FFFFFF";
+            }
+        }
+
+        public int QuantityToAdd
+        {
+            get
+            {
+                if (WP == null || WP.QuantityShelf <= 0)
+                    return 6;
+                if (WP.QuantityShelf > 10)
+                    return 10;
+
+                return WP.QuantityShelf;
+            }
+        }
+
+        public string NumberOfReplenishment { get; set; } = "0";
+        public string BarCodeInput { get; set; }
+
+        public string QuantityToAddText => $"+{QuantityToAdd}";
+        public PriceCheckVM(TypeDoc pTypeDoc, ForMVVM pForMVVM)
+        {
+            ForMVVM=pForMVVM;
+            bl.ClearWPH();
+            var r = db.GetCountScanCode();
+            IsVisDoubleScan = pTypeDoc.CodeDoc == 15;
+
+            if (Config.TypeUsePrinter == eTypeUsePrinter.StationaryWithCutAuto) PrintType = -1;
+
+            if (r != null)
+            {
+                AllScan = r.AllScan;
+                BadScan = r.BadScan;
+                LineNumber = r.LineNumber;
+                PackageNumber = r.PackageNumber;
+                OnClickAddPrintBlock(null, null);
+            }
+            if (!IsVisScan)
+                Config.BarCode = BarCode;
+
+        }
+            
+        void BarCode(string pBarCode) => FoundWares(pBarCode, false);
+
+        public void FoundWares(string pBarCode, bool pIsHandInput = false)
+        {
+            if (!String.IsNullOrWhiteSpace(pBarCode))
+            {
+                LineNumber++;
+                Config.OnProgress?.Invoke(0.2d);
+
+                WP = bl.FoundWares(pBarCode, PackageNumber, LineNumber, pIsHandInput, IsVisDoubleScan, IsOnline);
+                NumberOfReplenishment = "0";
+                OnPropertyChanged(nameof(QuantityToAdd));
+                OnPropertyChanged(nameof(QuantityToAddText));
+                if (WP != null)
+                {
+                    AllScan++;
+                    if (!WP.IsPriceOk)
+                        BadScan++;
+                    IsWareScaned = WP.StateDoubleScan;
+                }
+                if (Config.IsVibration)
+                {
+                    var duration = TimeSpan.FromMilliseconds(WP?.IsPriceOk == true ? 50 : 250);
+                    Vibration.Vibrate(duration);
+                }
+
+                Config.OnProgress?.Invoke(0.9d);
+                if (DeviceInfo.Platform != DevicePlatform.iOS) ForMVVM.Focused("BarCodeInput");
+            }
+            OnPropertyChanged(nameof(ListPrintBlockItems));
+        }
+
+        public void Dispose() { Config.BarCode -= BarCode; }
+
+        public void UnloadedEvent()
+        {
+            if (IsVisDoubleScan && bl.WPH != null) // При виході фіксувати останне не збережене в подвійному скануванні.
+                bl.SaveDoubleScan(bl.WPH.IsBarCode ? 101 : 102, bl.WPH, PackageNumber, LineNumber);
+        }
+
+        private void OnClickAddPrintBlock(object sender, EventArgs e)
+        {
+            PackageNumber++;
+            ListPrintBlockItems.Add(new PrintBlockItems() { PackageNumber = PackageNumber });
+        }
+
+        private void OnClickPrintBlock(object sender, EventArgs e)
+        {
+            var temp = PrintBlockItemsXaml.SelectedItem as PrintBlockItems;
+            if (IsEnabledPrint)
+                ForMVVM.DisplayAlert("Друк", bl.PrintPackage(PrintType, temp.PackageNumber, IsMultyLabel), "OK");
+        }
+
+        private void OnF2(object sender, EventArgs e)
+        {
+            IsVisRepl = !IsVisRepl;
+            if (IsVisRepl) ForMVVM.Focused("NumberOfReplenishment");
+        }
+
+        private void OnF4(object sender, EventArgs e) { IsOnline = !IsOnline; }
+
+        private void OnF5(object sender, EventArgs e) { IsMultyLabel = !IsMultyLabel; }
+
+       
+        private void BarCodeHandInput(object sender, EventArgs e)
+        {
+            var text = BarCodeInput;
+            FoundWares(text, true);
+        }
+
+        private void OnClickPrintOne(object sender, EventArgs e)
+        {
+            if (IsEnabledPrint && WP != null)
+                ForMVVM.DisplayAlert("Друк", bl.c.PrintHTTP(new[] { WP.CodeWares }), "OK");
+        }
+
+        private void OnUpdateReplenishment(object sender, EventArgs e)
+        {
+            decimal d;
+            if (decimal.TryParse(NumberOfReplenishment, out d))
+                db.UpdateReplenishment(LineNumber, d);
+        }
+
+        private void DoubleScanReact(object sender, EventArgs e)
+        {
+
+            if (IsWareScaned == eCheckWareScaned.PriceTagScaned || IsWareScaned == eCheckWareScaned.WareNotFit)//Відсутній товар
+            {
+                bl.SaveDoubleScan(102, WP, PackageNumber, LineNumber);
+                WP = null;
+                //IsWareScaned = eCheckWareScaned.Nothing;
+                //MessageDoubleScan = "Скануйте цінник чи товар";
+            }
+            else if (IsWareScaned == eCheckWareScaned.WareScaned || IsWareScaned == eCheckWareScaned.PriceTagNotFit)//Відсутній ціник
+            {
+                bl.SaveDoubleScan(101, WP, PackageNumber, LineNumber);
+                WP = null;
+                //IsWareScaned = eCheckWareScaned.Nothing;
+                //MessageDoubleScan = "Скануйте цінник чи товар";
+            }
+
+        }
+
+        
+        private void OnClearClicked(object sender, EventArgs e)
+        {
+            NumberOfReplenishment = "0";
+
+            OnUpdateReplenishment(null, null);
+        }
+
+        private void OnMinus1Clicked(object sender, EventArgs e)
+        {
+            ModifyValue(-1);
+        }
+
+        private void OnPlus1Clicked(object sender, EventArgs e)
+        {
+            ModifyValue(1);
+        }
+
+        private void OnPlusDynamicClicked(object sender, EventArgs e)
+        {
+            ModifyValue(QuantityToAdd);
+        }
+
+        private void ModifyValue(int delta)
+        {
+            if (int.TryParse(NumberOfReplenishment, out int currentVal))
+            {
+                int newVal = currentVal + delta;
+                NumberOfReplenishment = (newVal < 0 ? 0 : newVal).ToString();
+            }
+            else
+            {
+                NumberOfReplenishment = delta > 0 ? delta.ToString() : "0";
+            }
+            OnUpdateReplenishment(null, null);
+        }
+    }
+}
